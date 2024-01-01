0d9416b4b212e86871c1278cd685a77e6a597651
arshedgithub <arshedahmedmail@gmail.com>
2024-01-02 01:40:09 +0530

    add login route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags } from "./data";
+import jwt from "jsonwebtoken";
+
+import { sample_foods, sample_tags, sample_users } from "./data";
 
 const app = express();
-
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: ["http://localhost:4200"]
@@ -35,6 +37,17 @@ app.get('/api/foods/:foodId', (req,res) => {
     const food = sample_foods.find(food => food.id == foodId);
     res.send(food);
 });
+
+app.post('/api/users/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = sample_users.find(user => user.email === email && user.password === password);
+    if (user) res.send(generateTokenResponse(user));
+    else res.status(400).send("Incorrect Username or Password")
+});
+
+const generateTokenResponse = (user:any) => {
+    jwt.sign({ email: user.email, isAmdin: user.isAdmin}, "someRandomText", { expiresIn: "1d"})
+}
 
 const port = 5000;
 app.listen(port, () => console.log("website served on localhost:" + port));
